4703056d03c4c265e43c65d567c37a223a1e7a79
Sergey Melekhin <sergey@melekhin.me>
2018-12-12 12:57:30 +1000

    Day 7 part two :)

--- day7/10v4.py
+++ day7/10v4.py
@@ -2,24 +2,43 @@ import re
 
 
 def I(char):
-    return ord(char)-ord('A')
+    return ord(char)-ord('A')+1
 
 
-def A(i):
-    return chr(ord('A') + i)
+PROD = True
 
+MINTIME = 0
+WORKERS = 2
 
-def find(l, v):
-    i = -1
-    try:
-        i = l.index(v)
-    except ValueError:
-        i = -1
-    return i
+if PROD:
+    MINTIME = 60
+    WORKERS = 5
 
 
 steps = []
 distinct = set([])
+workers = [{"Working": 0, "Task": None} for _ in range(0, WORKERS)]
+
+
+def tick():
+    tasks = []
+    for worker in workers:
+        if worker["Task"]:
+            worker["Working"] += 1
+            if worker["Working"] == I(worker["Task"]) + MINTIME:
+                task = worker["Task"]
+                worker["Task"] = None
+                worker["Working"] = 0
+                tasks += task
+    print(workers)
+    return tasks
+
+
+def getFree():
+    for i in range(0, len(workers)):
+        if workers[i]["Task"] == None:
+            return i
+    return None
 
 
 def canput(x, seq):
@@ -42,7 +61,8 @@ Step B must be finished before step E can begin.
 Step D must be finished before step E can begin.
 Step F must be finished before step E can begin.
 '''
-        text = f.read()
+        if PROD:
+            text = f.read()
 
     for line in text.splitlines():
         m = p.match(line.strip())
@@ -54,8 +74,51 @@ Step F must be finished before step E can begin.
         steps.append((a, b))
 
     seq = []
+    seqWorkers = []
+    second = 0
     while len(distinct) > 0:
-        nextC = min(sorted([c for c in distinct if canput(c, seq)]))
+        notused = distinct.difference(seqWorkers)
+        print(notused)
+        validmoves = [c for c in notused if canput(c, seqWorkers)]
+        nextC = None
+        if len(validmoves) > 0:
+            nextC = min(validmoves)
+        if nextC is None:
+            c = tick()
+            second += 1
+            if len(c) > 0:
+                seqWorkers += c
+            continue
+
+        i = None
+        while True:
+            i = getFree()
+            if i is not None:
+                break
+            c = tick()
+            second += 1
+            if len(c) > 0:
+                seqWorkers += c
+
+        workers[i]["Task"] = nextC
+        workers[i]["Working"] = 0
         seq += nextC
+
         distinct.remove(nextC)
+
+    while True:
+        working = False
+        for i in range(0, WORKERS):
+            if workers[i]["Task"]:
+                working = True
+        if not working:
+            break
+
+        c = tick()
+        second += 1
+        if len(c) > 0:
+            seqWorkers += c
+
     print("".join(seq))
+    print("".join(seqWorkers))
+    print(second)
